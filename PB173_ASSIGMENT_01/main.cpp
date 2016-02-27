618dac3e82ea3993f4b9e9dc5cbf3259b099cafe
Kristian Jakubik <422430@mail.muni.cz>
2016-02-27 19:07:13 +0100

    SHA-512 function used

--- PB173_ASSIGMENT_01/main.cpp
+++ PB173_ASSIGMENT_01/main.cpp
@@ -8,27 +8,40 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <aes.h>
+#include <sha512.h>
 
 using namespace std;
 
-void read_file(char* path){
+void read_file(char* path, string &file_content){
     ifstream file;
     file.open(path);
     
-    string result;
     char c;
     while(file.get(c)){
-        result += c;
+        file_content += c;
     }
-    cout << result << endl;
+    cout << file_content << endl;
     file.close();
 }    
 
-int main(int argc, char** argv) {
+void create_hash(string &file_content, unsigned char* sha_result){
+    mbedtls_sha512((unsigned char*)file_content.c_str(), file_content.length(), sha_result, 0);   
+    cout << sha_result << endl;
+}
 
-    if(argc < 2)
+int main(int argc, char** argv){
+
+    if(argc < 2){
         cout << "Cannot read the file." << endl;
-    read_file(argv[1]);
+        return -1;
+    }
+    
+    string file_content;
+    unsigned char sha_result[64] = {""};
+    
+    read_file(argv[1], file_content);
+    create_hash(file_content, sha_result);
     return 0;
 }
 
