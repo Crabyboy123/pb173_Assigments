1f25b01b93eff2e2d279f2c0a9b9507f57539d49
Kristian Jakubik <422430@mail.muni.cz>
2016-02-28 21:06:39 +0100

    Added encryption, padding.

--- PB173_ASSIGMENT_01/main.cpp
+++ PB173_ASSIGMENT_01/main.cpp
@@ -15,70 +15,65 @@
 #include <ctr_drbg.h>
 #include <cstdlib> 
 #include <ctime>
+#include <string.h>
 #include <linux/random.h>
 
 using namespace std;
-
+#define IV "Dnes si dam caj" 
 mbedtls_aes_context aes;
 
+int input_length;
+unsigned char pad;
+
 unsigned char key[16];
-unsigned char iv[16];
 
-unsigned char input [128];
-unsigned char output[128];
-
-size_t input_len = 40;
-size_t output_len = 0;
-
-void read_file(char* path, string &file_content){
+void read_file(char* path, unsigned char**file_content){
     ifstream file;
     file.open(path);
-    
+    string tmp;
+ 
     char c;
     while(file.get(c)){
-        file_content += c;
+        tmp += c;
     }
-    cout << file_content << endl;
-    
+    cout << tmp << endl;
+    input_length = tmp.length();
+    if(input_length % 16)
+        pad = 16 - input_length % 16;
+    *file_content = new unsigned char[input_length + pad];
+    memcpy(*file_content, tmp.c_str(), input_length);
     file.close();
 }    
 
-void create_hash(string &file_content, unsigned char* sha_result){
-    mbedtls_sha512((unsigned char*)file_content.c_str(), file_content.length(), sha_result, 0);   
+void create_hash(unsigned char *file_content, unsigned char* sha_result){
+    mbedtls_sha512(file_content, input_length, sha_result, 0);   
     cout << sha_result << endl;
 }
 
 void generate_key(){
-    
-    /*mbedtls_ctr_drbg_context ctr_drbg;
-    mbedtls_entropy_context entropy;
-
-    int ret;
-    
-    mbedtls_entropy_init( &entropy );
-    mbedtls_ctr_drbg_init( &ctr_drbg ); 
-
-    if( ( ret = mbedtls_ctr_drbg_random( &ctr_drbg, key, 16 ) ) != 0 )
-    {
-        printf( " failed\n ! mbedtls_ctr_drbg_random returned -0x%04x\n", -ret );
-        return;
-    }
-    cout << "Kluce" << endl;
-    for(int i = 0; i < 16; i++) cout << key[i] << endl;*/
-
     srand((unsigned)time(0)); 
     for(int i = 0; i < 16; i++){
         key[i] = (rand()%256); 
     }
 }
 
-void encryption(string &file_content){
-    generate_key();
-    if( mbedtls_aes_setkey_enc(&aes, key, 128) ){
-        cout << "setkey_enc error" << endl;
-        exit( -1);
+void padding(unsigned char* file_content){
+    for(int i = 0; i < pad; i++){
+        file_content[input_length + i] = pad;
     }
-    
+}
+
+void encryption(unsigned char* file_content, unsigned char* en_output){
+    generate_key();
+    if (mbedtls_aes_setkey_enc(&aes, key, 128)){
+        cout << "setkey_enc error" << endl;
+        exit(-1);
+    }
+    padding(file_content);
+    if (mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_ENCRYPT, 16, (unsigned char*)IV, file_content, en_output)){
+        cout << "aes_crypt error" << endl;
+        exit(-1);
+    }
 }
 
 int main(int argc, char** argv){
@@ -88,12 +83,17 @@ int main(int argc, char** argv){
         return -1;
     }
     
-    string file_content;
+    unsigned char* file_content;
+    unsigned char* en_output;
     unsigned char sha_result[64] = {'\0'};
+    memset(sha_result, '\0', sizeof(unsigned char) * 64);
     
-    read_file(argv[1], file_content);
+    read_file(argv[1], &file_content);
+    en_output = new unsigned char[input_length];
     create_hash(file_content, sha_result);
-    encryption(file_content);
+    encryption(file_content, en_output);
+    delete file_content;
+    delete en_output;
     return 0;
 }
 
